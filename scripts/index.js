b7ffd29160c87ab341d153fcd05d8d038ed1d31d
Nafikov20 <snkr.nk95@gmail.com>
2023-11-05 14:22:36 +0300

    some edits

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,10 @@ document.addEventListener('DOMContentLoaded', function() {
     const openModalBtn = document.querySelectorAll('[data-modal]');
     const modal = document.querySelector('#payment-method-modal');
     const close = document.querySelector('#close-payment-modal');
+    const btnAddressPoint = document.querySelector('#address-point');
+    const btnAddressCourier = document.querySelector('#address-courier');
+    const shipAddressPoint = document.querySelector('.ship-address__wrapper-point');
+    const shipAddressCourier = document.querySelector('.ship-address__wrapper-courier');
 
     openModalBtn[0].addEventListener('click', () => {
         modal.showModal()
@@ -17,6 +21,20 @@ document.addEventListener('DOMContentLoaded', function() {
             modal.close();
         }
     });
+    btnAddressPoint.addEventListener('click', (e) => {
+        e.stopPropagation();
+        shipAddressPoint.classList.add('ship-address__hidden');
+        shipAddressCourier.classList.remove('ship-address__hidden');
+        btnAddressCourier.classList.remove('dailog__ship-btn-active');
+        btnAddressPoint.classList.add('dailog__ship-btn-active');
+    })
+    btnAddressCourier.addEventListener('click', (e) => {
+        e.stopPropagation();
+        shipAddressCourier.classList.add('ship-address__hidden');
+        shipAddressPoint.classList.remove('ship-address__hidden');
+        btnAddressCourier.classList.add('dailog__ship-btn-active');
+        btnAddressPoint.classList.remove('dailog__ship-btn-active');
+    })
 });
 
 
